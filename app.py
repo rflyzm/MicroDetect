673b2cc3692eb671e34dcb65e8bf4e04e27edf87
rflyzm <auliarafly736@gmail.com>
2025-06-19 23:06:44 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import streamlit as st
-import PIL
 from PIL import Image
-import numpy as np
 
-st.title ("MicroDetect AI")
-st.Write ("Upload gambar mikroskop kamu dan kami bantu deteksi penyakitnya (dummy AI)")
+st.title("🧫 MicroDetect AI")
+st.write("Upload gambar mikroskop kamu, dan kami bantu deteksi penyakitnya (dummy AI).")
 
-uploaded_file = st.file_uploader ("Upload gambar citra mikroskop", type=["jpg", "png", "jpeg"])
+uploaded_file = st.file_uploader("Upload gambar mikroskop:", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not none:
-    image = image.open(uploaded_file)
-    st.image(image, caption="Citra Mikroskop yang Diunggah", width = 300)
-
-    # Simulasi prediksi AI dimmy
-    st.Write("🔍 Menganalisis...")
-    st.success("✅ Hasil Deteksi : *Malaria Terdeteksi*")
-  else:
-    st.info("silahkan upload gambar untuk mulai.")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Citra Mikroskop yang Diupload", width=300)
+    st.write("🔍 Menganalisis...")
+    st.success("✅ Hasil Deteksi: Malaria Terdeteksi (Dummy)")
+else:
+    st.info("Silakan upload gambar dulu untuk mulai analisis.")
